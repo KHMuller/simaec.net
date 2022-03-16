bfa004cd1b20ed01e714245dae431ee1631a1be1
Karl-Heinz Müller <karlheinz@gmail.com>
2022-03-16 09:14:11 -0400

    Create Feedback.svelte

---
+++ webforms/svelte-app-feedback/src/Feedback.svelte
@@ -0,0 +1,7 @@
+<script>
+    import Feedback from "./FeedbackForm.svelte";
+    let modal;
+</script>
+
+<p class="modalShow"><button on:click={() => modal.show()}>Feedback</button></p>
+<Feedback bind:this={modal} />
